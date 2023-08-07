b05ba45cf31310e73688471eeed73f5438ab4d8e
kioteh-snowflake <141674735+kioteh-snowflake@users.noreply.github.com>
2023-08-07 14:19:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
