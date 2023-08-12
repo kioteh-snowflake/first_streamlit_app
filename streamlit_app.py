06fba924032381d4f615fe078a7d3ae0c0616f7f
kioteh-snowflake <141674735+kioteh-snowflake@users.noreply.github.com>
2023-08-12 10:28:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,13 +3,6 @@ import pandas
 import requests
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
-
 streamlit.title('My mom\'s new healthy diner')
 
 streamlit.header('Breakfast Menu')
@@ -39,4 +32,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 
 streamlit.dataframe(fruityvice_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("select * From fruit_load_list")
+my_data_row = my_cur.fetchone()
+streamlit.text("The fruit load list contains:")
+streamlit.text(my_data_row)
+
 
