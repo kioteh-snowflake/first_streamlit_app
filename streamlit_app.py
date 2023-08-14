d662d2dd9ad2e9db3f44ffe177269ba63eb9f2d8
kioteh-snowflake <141674735+kioteh-snowflake@users.noreply.github.com>
2023-08-14 13:49:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My mom\'s new healthy diner')
 
@@ -32,6 +33,8 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
 
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
